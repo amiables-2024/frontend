62c4880e620c85ff6ffe637aa146ae4c2be83020
amaiable <atmai2002@gmail.com>
2024-03-02 21:32:28 +1100

    Committing broken Kanban board, issue is the indexing needs to be unique for all tickets

--- src/components/KanbanBoard/KanbanBoard.tsx
+++ src/components/KanbanBoard/KanbanBoard.tsx
@@ -1,6 +1,35 @@
 import React, { useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import KanbanColumn from './KanbanColumn'
+import KanbanItem from '../KanbanItem/KanbanItem';
+import KanbanColumn from './KanbanColumn';
+
+const finalSpaceCharacters = [
+  {
+    id: 'gary',
+    name: 'Gary Goodspeed',
+    thumb: '/images/gary.png'
+  },
+  {
+    id: 'cato',
+    name: 'Little Cato',
+    thumb: '/images/cato.png'
+  },
+  {
+    id: 'kvn',
+    name: 'KVN',
+    thumb: '/images/kvn.png'
+  },
+  {
+    id: 'mooncake',
+    name: 'Mooncake',
+    thumb: '/images/mooncake.png'
+  },
+  {
+    id: 'quinn',
+    name: 'Quinn Ergon',
+    thumb: '/images/quinn.png'
+  }
+]
 
 function KanbanBoard() {
   const [characters, updateCharacters] = useState(finalSpaceCharacters);
@@ -16,10 +45,15 @@ function KanbanBoard() {
   }
 
   return (
-    <div>
-      <KanbanColumn />
-      <KanbanColumn />
-      <KanbanColumn />
+    <div className="App">
+      <header className="App-header">
+        <h1>Kanban Board</h1>
+        <DragDropContext onDragEnd={handleOnDragEnd} className="w-1/3">
+          <KanbanColumn />
+          <KanbanColumn />
+          <KanbanColumn />
+        </DragDropContext>
+      </header>
     </div>
   );
 }
