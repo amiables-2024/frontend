b18c12e4b7c43f6b8a42a18b48579c206c5fca48
ElijahAA <45959516+ElijahAA@users.noreply.github.com>
2024-03-02 18:57:26 +1100

    Finished sign up page.

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,61 +1,82 @@
-import React, { useState } from 'react';
-import './Form.css';
-import axios, { AxiosError } from 'axios';
+import React, {useState} from 'react';
+import { redirect } from "react-router-dom";
 
-const SignUp = () => {
+import styles from './SignUp.module.css';
+import logoImg from "../../assets/images/logo_name.svg";
+import restClient from "../../util/rest.util";
 
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+export default function SignUp() {
+    const [error, setError] = useState('')
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/auth/register`, );
-      console.log('SignUp Success:', response.data);
-      // TODO: Handle success, set session token and redirect to personal dashboard page
-      sessionStorage.setItem("accessToken", response.data.access_token);  
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        console.error('SignUp Error:', error.response?.data);
-        // TODO: Handle error, display some error message detailing what failed
-      } else {
-        // Handle non-Axios errors
-        console.error('SignUp Error:', error);
-      }
-    }
-  };
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+        const request = await restClient.post('/auth/register', {
+            data: {
+                name: name,
+                email: email,
+                password: password
+            }
+        });
 
-  return (
-      <div>
-          <form onSubmit={handleSubmit} className="form-container">
-              <h2>Sign Up</h2>
-              <input
-                  type="text"
-                  name="name"
-                  placeholder="name"
-                  value={name}
-                  onChange={(event) => setName(event.target.value)}
-              />
-              <input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)}
-              />
-              <input
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(event) => setPassword(event.target.value)}
-              />
-              <button type="submit">Sign Up</button>
-          </form>
-      </div>
+        if (!request.success) {
+            setError(request.data)
+            return
+        }
 
-  );
-};
+        localStorage.setItem("accessToken", request.data);
+        redirect("/dashboard");
+    };
 
-export default SignUp;
+    return (
+        <div className={styles.sign_in_page}>
+            <div className={styles.sign_in_container}>
+                <div className={styles.sign_up_box}>
+                    <form onSubmit={handleSubmit} className={styles.sign_in_form}>
+                        <a href={'/'}>
+                            <img src={logoImg} alt={"Sweet"}/>
+                        </a>
+                        <h2 className={styles.sign_up_header}>Let’s get you started with Sweet!</h2>
+                        {error && <p className={styles.error}>{error}</p>}
+                        <div className={styles.form_group}>
+                            <input
+                                type="text"
+                                placeholder="Enter your name"
+                                value={name}
+                                onChange={(event) => setName(event.target.value)}
+                            />
+                        </div>
+                        <div className={styles.form_group}>
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Enter your email"
+                                value={email}
+                                onChange={(event) => setEmail(event.target.value)}
+                            />
+                        </div>
+                        <div className={styles.form_group}>
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Enter your password"
+                                value={password}
+                                onChange={(event) => setPassword(event.target.value)}
+                            />
+                        </div>
+                        <div className={styles.btn_row}>
+                            <a href='/sign-in'>
+                                <button type="button">Sign In</button>
+                            </a>
+                            <button type="submit">Sign Up</button>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    );
+}
