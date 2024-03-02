9b58fd88ef7001add50614d065ade5a9844cf721
ElijahAA <45959516+ElijahAA@users.noreply.github.com>
2024-03-02 22:29:43 +1100

    Fixed compile error

--- src/components/ChatApp/ChatApp.tsx
+++ src/components/ChatApp/ChatApp.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [roomId, setroomId] = useState("");
 
   var socket: any;
-  socket = io(process.env.REACT_APP_BACKEND_URL);
+  socket = io(process.env.REACT_APP_BACKEND_URL!);
 
   const handleJoin = () => {
     if (userName !== "" && roomId !== "") {
