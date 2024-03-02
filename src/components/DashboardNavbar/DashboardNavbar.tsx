72172bda7a7f2844063a4f98d43c9171dce79670
ElijahAA <45959516+ElijahAA@users.noreply.github.com>
2024-03-02 20:42:54 +1100

    Completed project creation

--- src/components/DashboardNavbar/DashboardNavbar.tsx
+++ src/components/DashboardNavbar/DashboardNavbar.tsx
@@ -1,29 +1,84 @@
 import styles from "./DashboardNavbar.module.css";
 import logoImg from "../../assets/images/logo_name.svg";
 import {getAvatarUrl} from "../../util/misc.util";
-import {Navigate} from "react-router-dom";
+import {Link, Navigate, useNavigate} from "react-router-dom";
 import {User} from "../../util/types";
+import {useState} from "react";
+import Modal from "../Modal/Modal";
+import restClient from "../../util/rest.util";
 
 export default function DashboardNavbar() {
+
+    const navigate = useNavigate();
+
+    const [showModal, setShowModal] = useState(false);
+    const [error, setError] = useState('');
+    const [name, setName] = useState('');
+
     const cachedUser = localStorage.getItem("user");
     if (cachedUser == null)
-        return <Navigate to='/sign-in'/>
+        return <Navigate to='/sign-in'/>;
+
     const user: User = JSON.parse(cachedUser);
 
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+
+        const request = await restClient.post('/projects', {
+            data: {
+                name: name
+            }
+        });
+
+        if (!request.success) {
+            setError(request.data)
+            return
+        }
+
+        const projectId = request.data;
+
+        navigate(`/project/${projectId}`);
+    }
+
     return (
-        <div className={styles.navbar}>
-            <div>
-                <a className={styles.navbar_logo}>
-                    <img src={logoImg} alt={'Sweet Logo'}/>
-                </a>
-            </div>
-            <div className={styles.navbar_right}>
-                <button className={styles.project_create_btn}>Create New Project</button>
-                <div className={styles.avatar_group}>
-                    <p>{user.name}</p>
-                    <img src={getAvatarUrl(user.id)} alt={'Avatar'}/>
+        <>
+            <div className={styles.navbar}>
+                <div>
+                    <Link to='/' className={styles.navbar_logo}>
+                        <img src={logoImg} alt={'Sweet Logo'}/>
+                    </Link>
+                </div>
+                <div className={styles.navbar_right}>
+                    <button
+                        className={styles.project_create_btn}
+                        onClick={() => setShowModal(true)}
+                    >
+                        Create New Project
+                    </button>
+                    <div className={styles.avatar_group}>
+                        <p>{user.name}</p>
+                        <img src={getAvatarUrl(user.id)} alt={'Avatar'}/>
+                    </div>
                 </div>
             </div>
-        </div>
+            {showModal &&
+                <Modal closeModal={() => setShowModal(false)}>
+                    <form className={styles.create_box} onSubmit={handleSubmit}>
+                        <h1>Create Project</h1>
+                        {error && <p className="error">{error}</p>}
+                        <div className="form_group">
+                            <input
+                                placeholder={"Enter a project name"}
+                                value={name}
+                                onChange={(event) => setName(event.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <button className={styles.submit_btn} type="submit">Create Project</button>
+                        </div>
+                    </form>
+                </Modal>
+            }
+        </>
     )
 }
