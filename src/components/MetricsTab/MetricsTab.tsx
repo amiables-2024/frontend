3fc954d924faf92ce71acd5477f12295ecc50487
ElijahAA <45959516+ElijahAA@users.noreply.github.com>
2024-03-03 15:11:13 +1100

    Slight UI changes to metrics

--- src/components/MetricsTab/MetricsTab.tsx
+++ src/components/MetricsTab/MetricsTab.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import {Project} from "../../util/types";
 import behind_schedule from "../../assets/images/behind_schedule.png"
 import fresh_start from "../../assets/images/fresh_start.png"
+import styles from "./MetricsTab.module.css";
+
 const BEHIND_NAME = "Unihack";
 
 const MetricsTab = (props) => {
   return (
-    <div>
-      <img src={props.project.name == BEHIND_NAME ? behind_schedule : fresh_start} />
+    <div className={styles.metrics_tab}>
+      <img src={props.project.name == BEHIND_NAME ? behind_schedule : fresh_start} alt="Progress"/>
     </div>
   );
 }
